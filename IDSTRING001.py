b0b7cf6c76ba9b2f3a7ca905362efc1af331716c
Tuan Plus <Dongoctuan2002kk@gmail.com>
2022-09-26 12:22:08 +0700

    fix IDSTRING001

--- IDSTRING001.py
+++ IDSTRING001.py
@@ -4,12 +4,15 @@ if __name__ == '__main__':
     for k in range(t):
         s = input().split()
         s1 = s[2][1:-2]
-        s2 = s[5][1:-2]
-        # print(s1, s2)
+        s2 = s[5][1:-1]
+        print(s1, s2)
         if s1 == "" or s2 == "":
             print(0)
             continue
+        if s1 == s2:
+            print(1)
+            continue
         try:
-            print(1 if s2.index(s1) == 0 else s2.index(s1))
+            print(s2.index(s1))
         except:
             print(-1)
